50cf7e8f4f5bb66065ef19fb305bc0d9ed8e828b
rrslamerr <rrslamerr@yandex.ru>
2025-10-18 20:59:11 +0500

    Setup database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.routers import profiles
+from app.database import setup_database
+from .models import BaseModel
 
-app = FastAPI()
+app = FastAPI(title="Dating Profile Manager")
 app.include_router(profiles.router)
+
+
+@app.on_event("startup")
+async def on_startup():
+    await setup_database(BaseModel)
