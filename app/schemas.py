e79e1c38645d61270988bdaeff0d8246e1b6478b
rrslamerr <rrslamerr@yandex.ru>
2025-10-20 10:44:56 +0500

    update schemas

--- app/schemas.py
+++ app/schemas.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel, Field
 class ProfileBase(BaseModel):
     name: str
     age: int
-    description: str | None = Field(max_length=1000)
-    interests: str | None = Field(max_length=300)
+    description: str | None = Field(default=None, max_length=1000)
+    interests: str | None = Field(default=None, max_length=300)
 
 
 class ProfileCreate(ProfileBase):
