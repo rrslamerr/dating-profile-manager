4d6ddae3d2f409e519ba803ad23e28a816f6bb7a
rrslamerr <rrslamerr@yandex.ru>
2025-11-12 20:10:16 +0500

    add comments for endpoints

--- app/routers/profiles.py
+++ app/routers/profiles.py
@@ -8,16 +8,19 @@ router = APIRouter(prefix="/profiles", tags=["Profiles"])
 
 @router.post("/")
 async def create_profile(data: ProfileCreate, session: SessionDep):
+    """Создаёт новый профиль пользователя"""
     return await crud.create_profile(data, session)
 
 
 @router.get("/")
 async def get_profiles(session: SessionDep):
+    """Возвращает список всех профилей пользователей"""
     return await crud.get_profiles(session)
 
 
 @router.delete("/{profile_id}")
 async def delete_profile(profile_id: int, session: SessionDep):
+    """Удаляет профиль по id"""
     deleted = await crud.delete_profile(profile_id, session)
     if not deleted:
         raise HTTPException(status_code=404, detail="Profile not found")
