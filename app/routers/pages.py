c87579c7fc0551d03050b835e3fb2395c4154354
rrslamerr <rrslamerr@yandex.ru>
2025-11-17 12:30:57 +0500

    redesigned the main page

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from app.dependencies import SessionDep
+from app import crud
 
 router = APIRouter(tags=["Profiles"])
 templates = Jinja2Templates(directory="app/templates")
 
 
 @router.get("/")
-async def home(request: Request):
+async def home(request: Request, session: SessionDep):
     """Главная страница"""
-    return templates.TemplateResponse(request=request, name="home.html")
+    profiles = await crud.get_profiles(session)
+    return templates.TemplateResponse(
+        request=request, context={"profiles": profiles}, name="index.html"
+    )
